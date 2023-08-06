0dbe6a8849cf621e2ebc7c34af420d9daf551d6a
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-06 18:29:29 -0400

    attempt 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
 import Landing from '@/components/Landing/landing'
 import Image from 'next/image'
 
 export default function Home() {
 
-  const url = window.location.search;
+  const [url, setUrl] = useState<string | null>(null);
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    setUrl(`${pathname}?${searchParams}`);
+  }, [pathname, searchParams]);
 
   return (
     <Landing url={url} />
