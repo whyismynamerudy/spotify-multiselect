56ad21c9a01a410660b730fbd3e46ea883eeab25
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-06 02:11:51 -0400

    next step?

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -17,31 +17,29 @@ export async function GET(request: NextRequest) {
             code: ${code}
     `)
 
-    return NextResponse.json({ 'success': "ok" })
+    const credentials = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
 
-    // const credentials = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
+    const requestBody = querystring.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.REDIRECT_URI
+    });
 
-    // const requestBody = querystring.stringify({
-    //     grant_type: "authorization_code",
-    //     code: code,
-    //     redirect_uri: process.env.REDIRECT_URI
-    // });
-
-    // type HeadersType = {
-    //     'Content-Type': string;
-    //     Authorization: string;
-    // };
+    type HeadersType = {
+        'Content-Type': string;
+        Authorization: string;
+    };
       
-    // const headers: HeadersType = {
-    //     'Content-Type': 'application/x-www-form-urlencoded',
-    //     Authorization: `Basic ${credentials}`,
-    // };
+    const headers: HeadersType = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${credentials}`,
+    };
 
-    // console.log(`
-    //     Credentials: ${credentials},
-    //     RequestBody: ${requestBody},
-    //     Headers: ${headers}
-    // `)
+    console.log(`
+        Credentials: ${credentials},
+        RequestBody: ${requestBody},
+        Headers: ${headers}
+    `)
 
     // axios.post('https://accounts.spotify.com/api/token', {
     //     grant_type: 'authorization_code',
@@ -67,24 +65,26 @@ export async function GET(request: NextRequest) {
     //         Authorization: `Basic ${new (Buffer as any).from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
     //     },
     // })
-    // axios
-    // .post('https://accounts.spotify.com/api/token', requestBody, { headers })
-    // .then((response: any) => {
-    //     console.log("In Response Section of callback, gotten response: ", response.data)
-        // if (response.status === 200) {
+    axios
+    .post('https://accounts.spotify.com/api/token', requestBody, { headers })
+    .then((response: any) => {
+        console.log("In Response Section of callback, gotten response: ", response.data)
+        if (response.status === 200) {
 
-        //     const { access_token, token_type } = response.data;
-        //     const auth = `${token_type} ${access_token}`;
-        //     Cookies.set('Authorization', auth);
+            const { access_token, token_type } = response.data;
+            const auth = `${token_type} ${access_token}`;
+            Cookies.set('Authorization', auth);
 
-        //     return NextResponse.redirect('/profile');
+            return NextResponse.redirect('/profile');
 
-        // } else {
-        //     return NextResponse.json({ response });
-        // }
-    // })
-    // .catch((err: any) => {
-    //     console.log(`There has been an error in the callback, ${err}`)
-    //     return NextResponse.json({ err });
-    // })
+        } else {
+            return NextResponse.json({ response });
+        }
+    })
+    .catch((err: any) => {
+        console.log(`There has been an error in the callback, ${err}`)
+        return NextResponse.json({ err });
+    })
+
+    return NextResponse.json({ 'success': "ok" })
 }
