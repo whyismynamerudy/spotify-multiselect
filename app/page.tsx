a5eba52b9d97eed7959b2f486a871d9145f63496
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-06 18:44:29 -0400

    using useRotuer

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import { useEffect, useState } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
+import {useRouter} from 'next/router';
 import Landing from '@/components/Landing/landing'
 import Image from 'next/image'
 
 export default function Home() {
 
   const [url, setUrl] = useState<string>("");
+  const router = useRouter()
 
   const pathname = usePathname()
   const searchParams = useSearchParams()
@@ -17,6 +19,6 @@ export default function Home() {
   }, [pathname, searchParams]);
 
   return (
-    <Landing url={url} />
+    <Landing url={router.asPath} />
   )
 }
