626b02dad50d67e26899f204e88aab777f413165
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-07 03:16:00 -0400

    trying to fix logout button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import AuthProvider from '@/components/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
           {children}
-        </AuthProvider>
       </body>
     </html>
   )
