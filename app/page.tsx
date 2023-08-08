4ead6af62e65fb154a97bf9d979d3d7335913497
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-08 17:53:50 -0400

    atempting refresh token req

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,10 @@ export default function Home() {
   const access_token = params.get('access_token');
   const token_type = params.get('token_type');
   const expires_in = params.get('expires_in');
+  const stored_at = params.get('stored_at');
+  const refresh_token = params.get('refresh_token');
 
-  const queries = `?access_token=${access_token}&token_type=${token_type}&expires_in=${expires_in}`;
+  const queries = `?access_token=${access_token}&token_type=${token_type}&expires_in=${expires_in}&stored_at=${stored_at}&refresh_token=${refresh_token}`;
 
   const url = "https://spotify-multiselect.vercel.app/" + (access_token ? queries : "")
 
--- components/Landing/landing.tsx
+++ components/Landing/landing.tsx
@@ -25,6 +25,7 @@ export default function Landing({ url }: LandingProps) {
         const refresh_token = query.searchParams.get('refresh_token') || null;
 
         const refresh_req = async () => {
+            console.log("refresh req called");
             const newres = await axios.get(`https://spotify-multiselect.vercel.app/api/refresh_token?refresh_token=${refresh_token}`)
             setRes(newres)
         }
