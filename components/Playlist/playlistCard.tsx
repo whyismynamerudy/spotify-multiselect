6310c0cddd418dd6b78a0c699f2302f55560ad29
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-11 00:11:12 -0400

    dsfa

--- components/Playlist/playlistCard.tsx
+++ components/Playlist/playlistCard.tsx
@@ -5,7 +5,7 @@ export default function PlaylistCard({ id, images, name, owner }: Playlist) {
         <div className="h-32 w-[22rem] bg-gray-700 flex" id={id}>
             <img src={images[0].url} alt={name} className="m-2 w-28 h-28"/>
             <div className="flex justify-center items-center">
-                <h3 className=" text-slate-50">{name}</h3>
+                <h3 className=" text-slate-50 text-base">{name}</h3>
                 {/* <h5 className=" text-slate-50">The Genius of : {owner.display_name}</h5> */}
             </div>
         </div>
