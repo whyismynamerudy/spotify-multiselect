05b347635263abb0532f626ded2257e769865fc5
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-10 23:12:35 -0400

    trying to fix playlist retrieval pt1

--- components/Profile/profile.tsx
+++ components/Profile/profile.tsx
@@ -39,17 +39,17 @@ export default function ProfileComp() {
     }
 
     useEffect(()=>{
-        console.log('test is: ', test)
-        console.log('token is: ', token)
         if (!token) {
             console.log('token not detected')
             redirect('/')
         }
 
         getProfileData();
-        getPlaylistData();
-        
     }, [])
+
+    useEffect(() => {
+        getPlaylistData();
+    }, [user])
 
     return (
         <div className="h-screen min-h-screen">
@@ -58,7 +58,7 @@ export default function ProfileComp() {
             <div className="col-span-1 m-auto">
                 {user && <UserDetails user={user}/>}
             </div>
-            <div className="col-span-2 bg-green-500">
+            <div className="col-span-2">
                 <nav className="min-w-full flex flex-row-reverse">
                     <button className="text-slate-50 rounded-md bg-green-600 m-3 p-1" onClick={handleLogOut}>
                         Log Out
