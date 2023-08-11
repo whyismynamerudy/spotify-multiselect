758ecbac1aedf6eb8c7548464e3065661cdb1f6d
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-11 01:11:59 -0400

    default pfp

--- components/Profile/UserDetails/userdetails.tsx
+++ components/Profile/UserDetails/userdetails.tsx
@@ -9,7 +9,7 @@ export default function UserDetails({ user }: UserDetailsProps) {
     return(
         <>
             <div className="mb-4 m-auto">
-                <img src={user.images[1]?.url} alt="Profile" className="h-40 w-40 rounded-full" />
+                <img src={user.images[1]?.url || 'images/defaultpfp.jpeg'} alt="Profile" className="h-40 w-40 rounded-full" />
             </div>
             <div className="text-xl font-bold text-slate-50">{user.display_name}</div>
             <div className="text-slate-50 text-lg">{user.id}</div>
