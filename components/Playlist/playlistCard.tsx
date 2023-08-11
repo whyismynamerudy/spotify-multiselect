8f251e102a2df78f8cbee0033ca5a5c4292f3099
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-11 00:17:08 -0400

    removed embarrasing playlist

--- components/Playlist/playlistCard.tsx
+++ components/Playlist/playlistCard.tsx
@@ -2,8 +2,8 @@ import { Playlist } from "@/utils/types";
 
 export default function PlaylistCard({ id, images, name, owner }: Playlist) {
     return (
-        <div className="h-32 w-[22rem] bg-gray-700 flex" id={id}>
-            <img src={images[0].url} alt={name} className="m-2 w-28 h-28"/>
+        <div className="h-40 w-[24rem] bg-gray-700 flex" id={id}>
+            <img src={images[0].url} alt={name} className="m-2 w-36 h-36"/>
             <div className="flex justify-center items-center">
                 <h3 className=" text-slate-50 text-base">{name}</h3>
                 {/* <h5 className=" text-slate-50">The Genius of : {owner.display_name}</h5> */}
