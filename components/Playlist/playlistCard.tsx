1a911b8b4a8a91f491b2c46f6dd530d77bfdd34c
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-11 00:50:46 -0400

    added missing profile pic error

--- components/Playlist/playlistCard.tsx
+++ components/Playlist/playlistCard.tsx
@@ -12,8 +12,8 @@ export default function PlaylistCard({ id, images, name, owner, onPlaylistClick 
 
     return (
         <div className="h-40 w-[24rem] bg-gray-700 flex cursor-pointer" id={id} onClick={handlePlaylistClick}>
-            <img src={images[0].url} alt={name} className="m-2 w-36 h-36"/>
-            <div className="flex flex-col justify-center items-center">
+            <img src={images[0]?.url} alt={name} className="m-2 w-36 h-36"/>
+            <div className="flex flex-col justify-center items-center w-full">
                 <h3 className=" text-slate-50 text-base">{name}</h3>
                 <h5 className=" text-gray-400 text-sm">The Genius of : {owner.display_name}</h5>
             </div>
