0f4c70377a263a94e12ab933bd3cbd789763b9b4
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-08-23 14:23:03 -0400

    added rick roll

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -33,7 +33,7 @@ export default function Login({ url }: LoginProps) {
                 <iframe 
                     width="853" 
                     height="480" 
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1" 
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
