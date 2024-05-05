c901d3378f51f2f7ea430896b70eb242818888fa
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-04 22:09:46 -0400

    playlist link?

--- components/Playlist/playlistCard.tsx
+++ components/Playlist/playlistCard.tsx
@@ -1,7 +1,7 @@
 import { Playlist } from "@/utils/types";
 
 
-export default function PlaylistCard({ id, images, name, owner }: Playlist) {
+export default function PlaylistCard({ id, images, name, owner, external_urls }: Playlist) {
 
     return (
         <div className="h-40 w-[24rem] bg-gray-700 flex cursor-pointer rounded-lg" id={id}>
@@ -9,7 +9,11 @@ export default function PlaylistCard({ id, images, name, owner }: Playlist) {
             <div className="flex flex-col justify-center items-center w-full">
                 <h3 className=" text-slate-50 text-base">{name}</h3>
                 <h5 className=" text-gray-400 text-sm">The Genius of : {owner.display_name}</h5>
-                <div className="bottom-0 flex items-center justify-center">Open in <img className="w-1/2" src="/SpotifyLogo.png"/></div>
+                <div className="bottom-0 flex items-center justify-center">
+                    <a href={external_urls.spotify} target="_blank">
+                     Open in <img className="w-1/2" src="/SpotifyLogo.png"/>
+                    </a>
+                </div>
             </div>
         </div>
     )
