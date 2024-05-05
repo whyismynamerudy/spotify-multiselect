40ba7befe8d94fa46f66ba9d3258ca77510844b2
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-04 22:52:51 -0400

    aaa

--- components/Playlist/playlistCard.tsx
+++ components/Playlist/playlistCard.tsx
@@ -6,12 +6,10 @@ export default function PlaylistCard({ id, images, name, owner, external_urls }:
     return (
         <div className="h-40 w-[24rem] bg-gray-700 flex cursor-pointer rounded-lg" id={id}>
             <img src={images[0]?.url} alt={name} className="m-2 w-36 h-36"/>
-            <div className="flex flex-col justify-between items-center w-full">
-                <div>
-                    <h3 className=" text-slate-50 text-base">{name}</h3>
-                    <h5 className=" text-gray-400 text-sm">The Genius of : {owner.display_name}</h5>
-                </div>
-                <div className="bottom-0 flex items-center justify-center mt-auto">
+            <div className="flex flex-col justify-center items-center w-full">
+                <h3 className=" text-slate-50 text-base">{name}</h3>
+                <h5 className=" text-gray-400 text-sm">The Genius of : {owner.display_name}</h5>
+                <div className="flex items-center justify-center mt-4">
                     <a href={external_urls.spotify} target="_blank" className="flex items-center justify-center">
                      Open in <img className="w-1/2" src="/SpotifyLogo.png"/>
                     </a>
