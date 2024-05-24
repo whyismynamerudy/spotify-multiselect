fdd6e0a0898374935d4261e7cc7276ab5c041039
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-24 16:38:41 -0400

    login change

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { redirect } from 'next/navigation'
-import { useRouter } from 'next/router';
+// import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react'
 const querystring = require('querystring');
 
